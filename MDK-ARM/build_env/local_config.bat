2355aefdc25c0b52076654a25c908f6e19032be8
TuanTA <tuantuksnb01@gmail.com>
2024-08-01 17:13:34 +0700

    Remove .lnp file

--- MDK-ARM/build_env/local_config.bat
+++ MDK-ARM/build_env/local_config.bat
@@ -2,5 +2,5 @@
 @SET KEIL_ARMCLANG="C:\Users\HP\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\ArmClang.exe"
 @SET KEIL_ARMLINK="C:\Users\HP\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\ArmLink"
 @SET KEIL_FROMELF="C:\Users\HP\AppData\Local\Keil_v5\ARM\ARMCLANG\Bin\fromelf.exe"
-@SET CMSIS_CORE_INC=C:/Users/HP/AppData/Local/Arm/Packs/ARM/CMSIS/6.1.0/CMSIS/Core/Include
-@SET MCU_DEVICE_INC=C:/Users/HP/AppData/Local/Arm/Packs/Keil/STM32F1xx_DFP/2.4.1/Device/Include
+@SET CMSIS_CORE_INC_PATH=C:/Users/HP/AppData/Local/Arm/Packs/ARM/CMSIS/6.1.0/CMSIS/Core/Include
+@SET MCU_DEVICE_INC_PATH=C:/Users/HP/AppData/Local/Arm/Packs/Keil/STM32F1xx_DFP/2.4.1/Device/Include
